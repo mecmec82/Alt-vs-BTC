61c2dad38a14d4721338175ac206b50037a892bc
mecmec82 <73520925+mecmec82@users.noreply.github.com>
2024-10-31 21:20:03 +0000

    Update ALTvsBTC.py
    fix rows chart issue

--- ALTvsBTC.py
+++ ALTvsBTC.py
@@ -70,7 +70,7 @@ rollingAverageData = rollingAverageData[-NumPoints:]
 
 # Plots
 numAssets=len(closeData.columns)
-numRows=int(numAssets/2)+1
+numRows=int(numAssets/2)
 
 fig, axes = plt.subplots(numRows, 2, figsize=(10, 10),sharex=True)
 fig.subplots_adjust(wspace=0.3,hspace=0)
