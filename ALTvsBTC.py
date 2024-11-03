4230d7f701d4bef4553f955ccd6b9782b459a565
mecmec82 <73520925+mecmec82@users.noreply.github.com>
2024-11-03 20:27:23 +0000

    Update ALTvsBTC.py

--- ALTvsBTC.py
+++ ALTvsBTC.py
@@ -8,7 +8,6 @@ import seaborn as sns
 
 @st.cache_data
 def getDataCCXT(ID, start, end):
-    st.write("getDataCCXT")
     exchange = ccxt.coinbase()
     data = exchange.fetch_ohlcv(ID, '2h')
     data = pd.DataFrame(data)
@@ -17,6 +16,70 @@ def getDataCCXT(ID, start, end):
     data['Date'] = pd.to_datetime(data['Date'], unit='ms')
     data.set_index('Date', drop=False, inplace=True)
     return data
+
+def fetch_data():
+    now = datetime.now()
+    end = (now - timedelta(hours=0.5)).strftime("%Y-%m-%d %H:%M:%S")
+    start = (now - timedelta(hours=50)).strftime("%Y-%m-%d %H:%M:%S")
+
+    # Get BTC data
+    try:
+        btcData = getDataCCXT("BTC/USD", start, end)['Close']
+    except:
+        st.write("Failed to retrieve BTC data")
+        return None
+
+    # Get alt data
+    closeData = pd.DataFrame()
+    for asset in st.session_state.assets:
+        assetName = str(asset).split("/")[0]
+        try:
+            closeData[assetName] = getDataCCXT(asset, start, end)['Close']
+        except:
+            st.write("Failed to retrieve data for ticker: ", asset)
+
+    # Reference to BTC
+    closeData = closeData.div(btcData, axis=0)
+    return closeData
+
+def plot_data(closeData, SMA1, SMA2, NumPoints):
+    # Create SMA dataframe
+    rollingAverageData1 = closeData.rolling(window=SMA1).mean()
+    rollingAverageData2 = closeData.rolling(window=SMA2).mean()
+
+    # Trim to desired timeframe
+    closeData = closeData[-NumPoints:]
+    rollingAverageData1 = rollingAverageData1[-NumPoints:]
+    rollingAverageData2 = rollingAverageData2[-NumPoints:]
+
+    # Plots
+    numAssets = len(closeData.columns)
+    numRows = numAssets
+
+    fig, axes = plt.subplots(numRows, figsize=(3, 20), sharex=True)
+
+    fig.subplots_adjust(wspace=0.3, hspace=0)
+    fig.suptitle('Altcoins vs BTC')
+
+    for index, asset in enumerate(closeData.columns):
+        x = rollingAverageData1.index
+        y1 = rollingAverageData1[asset]
+        y2 = rollingAverageData2[asset]
+
+        ax = sns.lineplot(ax=axes[index], data=rollingAverageData1, x='Date', y=asset, color="blue")
+        ax = sns.lineplot(ax=axes[index], data=rollingAverageData2, x='Date', y=asset, color="orange")
+        ax.fill_between(x, y1, y2, where=(y1 > y2), color='green', alpha=0.2, interpolate=True)
+        ax.fill_between(x, y1, y2, where=(y1 <= y2), color='red', alpha=0.2, interpolate=True)
+
+        if rollingAverageData1[asset][-1] > rollingAverageData2[asset][-1]:
+            ax.yaxis.label.set_color('green')
+            ax.tick_params(axis='y', colors='green')
+            ax.spines['left'].set_color('green')
+
+    fig.autofmt_xdate(rotation=90)
+
+    # Displaying in Streamlit
+    st.pyplot(fig, use_container_width=False)
 
 # Sidebar controls
 st.sidebar.header('Settings')
@@ -29,67 +92,13 @@ assets = st.sidebar.multiselect('Select Assets',
                                  'SEI/USD'], 
                                 default=['ETH/USD', 'SOL/USD', 'SUI/USD', 'AVAX/USD'])
 
-st.write('Fetching data...')
+if 'assets' not in st.session_state:
+    st.session_state.assets = assets
 
-data = np.nan
+if st.sidebar.button('Fetch Data'):
+    st.session_state.closeData = fetch_data()
 
-# Set up start and finish window
-now = datetime.now()
-end = (now - timedelta(hours=0.5)).strftime("%Y-%m-%d %H:%M:%S")
-start = (now - timedelta(hours=50)).strftime("%Y-%m-%d %H:%M:%S")
-
-# Get BTC data
-try:
-    btcData = getDataCCXT("BTC/USD", start, end)['Close']
-except:
-    st.write("Failed to retrieve BTC data")
-
-# Get alt data
-closeData = pd.DataFrame()
-for asset in assets:
-    assetName = str(asset).split("/")[0]
-    try:
-        closeData[assetName] = getDataCCXT(asset, start, end)['Close']
-    except:
-        st.write("Failed to retrieve data for ticker: ", asset)
-
-# Reference to BTC
-closeData = closeData.div(btcData, axis=0)
-
-# Create SMA dataframe
-rollingAverageData1 = closeData.rolling(window=SMA1).mean()
-rollingAverageData2 = closeData.rolling(window=SMA2).mean()
-
-# Trim to desired timeframe
-closeData = closeData[-NumPoints:]
-rollingAverageData1 = rollingAverageData1[-NumPoints:]
-rollingAverageData2 = rollingAverageData2[-NumPoints:]
-
-# Plots
-numAssets = len(closeData.columns)
-numRows = numAssets
-
-fig, axes = plt.subplots(numRows, figsize=(3, 20), sharex=True)
-
-fig.subplots_adjust(wspace=0.3, hspace=0)
-fig.suptitle('Altcoins vs BTC')
-
-for index, asset in enumerate(closeData.columns):
-    x = rollingAverageData1.index
-    y1 = rollingAverageData1[asset]
-    y2 = rollingAverageData2[asset]
-
-    ax = sns.lineplot(ax=axes[index], data=rollingAverageData1, x='Date', y=asset, color="blue")
-    ax = sns.lineplot(ax=axes[index], data=rollingAverageData2, x='Date', y=asset, color="orange")
-    ax.fill_between(x, y1, y2, where=(y1 > y2), color='green', alpha=0.2, interpolate=True)
-    ax.fill_between(x, y1, y2, where=(y1 <= y2), color='red', alpha=0.2, interpolate=True)
-
-    if rollingAverageData1[asset][-1] > rollingAverageData2[asset][-1]:
-        ax.yaxis.label.set_color('green')
-        ax.tick_params(axis='y', colors='green')
-        ax.spines['left'].set_color('green')
-
-fig.autofmt_xdate(rotation=90)
-
-# Displaying in Streamlit
-st.pyplot(fig, use_container_width=False)
+if 'closeData' in st.session_state:
+    plot_data(st.session_state.closeData, SMA1, SMA2, NumPoints)
+else:
+    st.write('Click "Fetch Data" to load the data.')
