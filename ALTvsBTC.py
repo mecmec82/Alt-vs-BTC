a1db650862be3c4cb8c18e13d864ca538405f372
mecmec82 <73520925+mecmec82@users.noreply.github.com>
2024-11-06 13:01:53 +0000

    Update ALTvsBTC.py

--- ALTvsBTC.py
+++ ALTvsBTC.py
@@ -35,7 +35,6 @@ def fetch_data(timeframe):
     my_bar = st.progress(0, text=progress_text)
     percent_complete = 0
     percentage_per_asset = int(100/len(st.session_state.assets))
-    st.write(percentage_per_asset)
     closeData = pd.DataFrame()
     for asset in st.session_state.assets:
         percent_complete += percentage_per_asset
